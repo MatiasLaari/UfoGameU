b52ea339f53e929476923c4b428ce2c96c87e712
MatiasLaari <97948223+MatiasLaari@users.noreply.github.com>
2024-11-14 17:59:54 +0200

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float _horizontalInput = 0;
+    private float _verticalInput = 0;
+    public int movementSpeed = 0;
+    public int rotationSpeed = 0;
+    Rigidbody2D rb2D;
+
+    private void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        GetPlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+        RotatePlayer();
+    }
+
+    private void GetPlayerInput()
+    {
+        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        _verticalInput = Input.GetAxisRaw("Vertical");
+    }
+    private void MovePlayer()
+    {
+       rb2D.velocity = transform.right * Mathf.Clamp01(_verticalInput) * movementSpeed;
+    }
+
+    private void RotatePlayer()
+    {
+        float rotation = -_horizontalInput * rotationSpeed;
+        transform.Rotate(Vector3.forward * rotation);
+    }
+}

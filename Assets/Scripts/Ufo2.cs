743c2432e090bdf7a49e62f8d3604f8afb0b2a83
MatiasLaari <97948223+MatiasLaari@users.noreply.github.com>
2024-12-17 13:05:10 +0200

    viiminen Commit peli on viimeistelty

--- Assets/Scripts/Ufo2.cs
+++ Assets/Scripts/Ufo2.cs
@@ -2,30 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class bullets : MonoBehaviour
+public class Ufo2 : MonoBehaviour
 {
     public GameObject explosionPrefab;
     private PointManager pointManager; // ScoreMagerin koodia
 
-    // Update is called once per frame
-    void Start()
-    {
-        pointManager = GameObject.Find("PointManager").GetComponent<PointManager>(); // ScoreManagerin koodia
-    }
-    void Update()
-    {
-        
-    }
+    public int deathCounter;// ufo2 osuma laskuri
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.tag == "Enemy")
+
+        if (collision.gameObject.CompareTag("Enemy2"))
+        {
+            deathCounter += 1;
+
+        }
+
+        if (deathCounter == 2)
         {
             GameObject newExplosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity);
             Destroy(newExplosion, 3f);
             Destroy(collision.gameObject);
-            pointManager.UpdateScore(1);// t‰m‰ lis‰‰ ScoreManageriiin pisteen
+            pointManager.UpdateScore(5);// t‰m‰ lis‰‰ ScoreManageriiin pisteen
             Destroy(gameObject);
-            
         }
     }
 }
